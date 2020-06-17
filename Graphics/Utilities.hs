d3752828fa3aba19d8cf127583456dd68e626a2a
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 05:08:35 -0500

    Update Utilities.hs
    Creates a function, to create a Vulkan API version number

--- Graphics/Utilities.hs
+++ Graphics/Utilities.hs
@@ -1,3 +1,17 @@
 {-# LANGUAGE Safe #-}
 
 module Graphics.Utilities where
+
+
+import Data.Bits (shiftL, (.|.))
+import Data.Word (Word32)
+
+
+type Major      = Word32
+type Minor      = Word32
+type Patch      = Word32
+type Version    = Word32
+
+
+makeAPI :: Major -> Minor -> Patch -> Version
+makeAPI major minor patch = shiftL major 22 .|. shiftL minor 12 .|. patch
