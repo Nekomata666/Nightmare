6155c2ac5b1d066b0738730c48e240da6c1c0cee
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 05:33:27 -0500

    Update Enumerations.hs
    Adds enum structureTypeApplicationInfo in VkStructureType

--- Graphics/Vulkan/Enumerations.hs
+++ Graphics/Vulkan/Enumerations.hs
@@ -1,3 +1,12 @@
 {-# LANGUAGE Safe #-}
 
 module Graphics.Vulkan.Enumerations where
+
+
+-- Vulkan newtypes
+newtype VkStructureType = VkStructureType { unVkStructureType :: Int32 }
+    deriving (Eq)
+
+
+structureTypeApplicationInfo                                  :: VkStructureType
+structureTypeApplicationInfo                                  = VkStructureType 0
