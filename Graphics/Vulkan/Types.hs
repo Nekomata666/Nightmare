a9606e508e68ad33946f0581d0f2970fbfe81df3
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 06:17:08 -0500

    Update Types.hs

--- Graphics/Vulkan/Types.hs
+++ Graphics/Vulkan/Types.hs
@@ -5,6 +5,23 @@ module Graphics.Vulkan.Types where
 
 import Data.Word (Word32)
 
+import Foreign.C.Types (CSize)
+import Foreign.Ptr (Ptr, FunPtr)
+
 
 -- Vulkan Type aliases.
 type VkFlags    = Word32
+type VkHandle   = Word64
+
+
+-- Vulkan Handles
+newtype VkInstance = VkInstance { unVkInstance :: VkHandle }
+
+
+-- Vulkan function pointers.
+type PFN_vkVoidFunction = FunPtr (IO ())
+type PFN_vkAllocationFunction = FunPtr (Ptr Void -> CSize -> CSize -> VkSystemAllocationScope -> IO (Ptr Void))
+type PFN_vkReallocationFunction = FunPtr (Ptr Void -> Ptr Void -> CSize -> CSize -> VkSystemAllocationScope -> IO (Ptr Void))
+type PFN_vkFreeFunction = FunPtr (Ptr Void -> Ptr Void -> IO ())
+type PFN_vkInternalAllocationNotification = FunPtr (Ptr Void -> CSize -> VkInternalAllocationType -> VkSystemAllocationScope -> IO ())
+type PFN_vkInternalFreeNotification = FunPtr (Ptr Void -> CSize -> VkInternalAllocationType -> VkSystemAllocationScope -> IO ())
