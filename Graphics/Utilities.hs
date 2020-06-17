a156aa5b353c28c02a6639571af4162ef67d0a96
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 07:20:33 -0500

    Update Utilities.hs

--- Graphics/Utilities.hs
+++ Graphics/Utilities.hs
@@ -5,6 +5,11 @@ module Graphics.Utilities where
 
 import Data.Bits (shiftL, (.|.))
 import Data.Word (Word32)
+import Data.Maybe
+
+import Foreign
+import Foreign.C.Types
+import Foreign.C.String
 
 
 type Major      = Word32
@@ -12,6 +17,35 @@ type Minor      = Word32
 type Patch      = Word32
 type Version    = Word32
 
+
+cast :: (Read a, Show b) => b -> a
+cast a = read $ show a
+
+fromMaybeIO :: Storable a => Maybe a -> IO (Ptr a)
+fromMaybeIO m
+    | isNothing m = return nullPtr
+    | otherwise   = alloca $ \p -> do
+        poke p $ fromJust m
+        return p
+
+fromMaybeListIO :: (Num a, Show a, Storable b) => a -> Maybe [b] -> IO (Ptr b)
+fromMaybeListIO c m
+    | isNothing m = return nullPtr
+    | otherwise   = allocaArray i $ \p -> do
+        pokeArray p $ fromJust m
+        return p
+    where
+        i = cast c
+
+fromMaybeStringListIO :: (Num a, Show a) => a -> Maybe [String] -> IO (Ptr CString)
+fromMaybeStringListIO c m
+    | isNothing m = return nullPtr
+    | otherwise   = allocaArray i $ \p -> do
+        m' <- stringListToCStringList $ fromJust m
+        pokeArray p m'
+        return p
+    where
+        i = cast c
 
 makeAPI :: Major -> Minor -> Patch -> Version
 makeAPI major minor patch = shiftL major 22 .|. shiftL minor 12 .|. patch
