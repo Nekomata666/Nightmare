48e7e55d925924764b301de9eae3fb27e448f257
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 05:44:28 -0500

    Update Vulkan.hs
    Now creates a VkApplicationInfo

--- Graphics/Vulkan.hs
+++ Graphics/Vulkan.hs
@@ -3,7 +3,15 @@
 module Graphics.Vulkan where
 
 
+import Foreign.Ptr (nullPtr)
+
+import Graphics.Utilities
+
+import Graphics.Vulkan.Instance
+
+
 initialize :: IO ()
 initialize = do
     let api = makeAPI 1 2 141
+    app <- createVkApplicationInfo nullPtr "Nightmare" 0 "Nightmare" 0 api
     return ()
