48471f687015176b179a585e3ddab2b3e88c1c44
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 06:02:35 -0500

    Update Vulkan.hs
    Now creates a VkInstanceCreateInfo

--- Graphics/Vulkan.hs
+++ Graphics/Vulkan.hs
@@ -14,4 +14,8 @@ initialize :: IO ()
 initialize = do
     let api = makeAPI 1 2 141
     app <- createVkApplicationInfo nullPtr "Nightmare" 0 "Nightmare" 0 api
+    vkI <- createVkInstanceCreateInfo nullPtr 0 (Just app) 1
+        (Just ["VK_LAYER_KHRONOS_validation"]) 2
+        (Just ["VK_EXT_debug_report", "VK_KHR_surface"])
+    
     return ()
