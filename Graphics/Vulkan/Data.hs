89af9842098a74bb784d18e366edb9d346144958
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 05:25:12 -0500

    Update Data.hs
    Adds VkApplicationInfo data type

--- Graphics/Vulkan/Data.hs
+++ Graphics/Vulkan/Data.hs
@@ -1,3 +1,23 @@
 {-# LANGUAGE DuplicateRecordFields, Safe #-}
 
 module Graphics.Vulkan.Data where
+
+
+import Data.Void (Void)
+import Data.Word (Word8, Word32)
+
+import Foreign
+import Foreign.C.String
+
+import Graphics.Vulkan.Types
+
+
+data VkApplicationInfo = VkApplicationInfo{
+    sType               :: VkStructureType,
+    next                :: Ptr Void,
+    applicationName     :: CString,
+    applicationVersion  :: Word32,
+    engineName          :: CString,
+    engineVersion       :: Word32,
+    apiVersion          :: Word32
+}
