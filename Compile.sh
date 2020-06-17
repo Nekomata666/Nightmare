7a170b41e74d6d17dd707ad4db3a5cc204d58794
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 01:26:56 -0500

    Update Compile.sh
    Fix library linking

--- Compile.sh
+++ Compile.sh
@@ -2,5 +2,5 @@ warnings="-Wall -Wcompat -Wname-shadowing"
 flags="-XSafe -O2 -dynamic -threaded"
 rts="-rtsopts"
 sdl2="-lSDL2"
-vulkan="-lvulkan -lVkLayer_core_validation"
-ghc Main.hs $warnings $flags $vulkan $rts -outputdir ./Build/Release
+vulkan="-lvulkan -lVkLayer_khronos_validation"
+ghc Main.hs $warnings $flags $sdl2 $vulkan $rts -outputdir ./Build/Release
