0341fe48e559b189a9529403cc3094cba375ca25
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-17 00:38:06 -0500

    Adds SDL_Quit

--- SDL2/SDL2.hs
+++ SDL2/SDL2.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module SDL2.SDL2 where
+module SDL2.SDL2 (sdl2Init, sdl2Quit) where
 
 
 import Data.Int     (Int32)
@@ -12,5 +12,11 @@ import Foreign
 foreign import ccall unsafe "SDL_Init"
     c_SDL_Init :: Word32 -> IO Int32
 
+foreign import ccall unsafe "SDL_Quit"
+    c_SDL_Quit :: IO ()
+
 sdl2Init :: Word32 -> IO Int32
 sdl2Init = c_SDL_Init
+
+sdl2Quit :: IO ()
+sdl2Quit = c_SDL_Quit
