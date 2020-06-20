9bf26697477d10bb6f5ff1ecbcbe219cdfc1f3cf
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-20 02:46:01 -0500

    Uses vkCreateInstance in Vulkan.hs

--- Graphics/Vulkan.hs
+++ Graphics/Vulkan.hs
@@ -13,9 +13,10 @@ import Graphics.Vulkan.Instance
 initialize :: IO ()
 initialize = do
     let api = makeAPI 1 2 141
-    app <- createVkApplicationInfo nullPtr "Nightmare" 0 "Nightmare" 0 api
-    vkI <- createVkInstanceCreateInfo nullPtr 0 (Just app) 1
+    app     <- createVkApplicationInfo nullPtr "Nightmare" 0 "Nightmare" 0 api
+    vkInfo  <- createVkInstanceCreateInfo nullPtr 0 (Just app) 1
         (Just ["VK_LAYER_KHRONOS_validation"]) 2
         (Just ["VK_EXT_debug_report", "VK_KHR_surface"])
-    
+    vkIn <- vkCreateInstance vkInfo
+
     return ()
