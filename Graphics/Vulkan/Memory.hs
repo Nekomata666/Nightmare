95c5b7dc3fab2b7bbc5313056027ead0cb62d0a7
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-06-23 22:55:49 -0500

    Adds vkAllocateMemory

--- Graphics/Vulkan/Memory.hs
+++ Graphics/Vulkan/Memory.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module Graphics.Vulkan.Memory (vkCreateMemoryAllocateInfo) where
+module Graphics.Vulkan.Memory (vkAllocateMemory, vkCreateMemoryAllocateInfo) where
 
 
 import Data.Void (Void)
@@ -14,6 +14,17 @@ import Graphics.Vulkan.Data
 import Graphics.Vulkan.Enumerations
 import Graphics.Vulkan.Types
 
+
+foreign import ccall unsafe "vkAllocateMemory"
+    c_vkAllocateMemory :: VkDevice -> Ptr VkMemoryAllocateInfo -> Ptr VkAllocationCallbacks -> Ptr
+        VkDeviceMemory -> IO VkResult
+
+vkAllocateMemory :: VkDevice -> VkMemoryAllocateInfo -> IO VkDeviceMemory
+vkAllocateMemory d i = alloca $ \pInfo ->
+    alloca $ \pMem -> do
+        poke pInfo i
+        _ <- c_vkAllocateMemory d pInfo nullPtr pMem
+        peek pMem
 
 vkCreateMemoryAllocateInfo :: Ptr Void -> VkDeviceSize -> Word32 -> VkMemoryAllocateInfo
 vkCreateMemoryAllocateInfo = VkMemoryAllocateInfo structureTypeMemoryAllocateInfo
