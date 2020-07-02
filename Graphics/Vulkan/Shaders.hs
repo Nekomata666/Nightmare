6227e933427da2566041207c0cfceae73b1da684
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-02 11:49:30 -0500

    Creates Shaders.hs

---
+++ Graphics/Vulkan/Shaders.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE ForeignFunctionInterface, Safe #-}
+
+module Graphics.Vulkan.Shaders () where
+
+
+import Data.Void (Void)
+
+import Foreign
+
+import Graphics.Utilities
+
+import Graphics.Vulkan.Data
+import Graphics.Vulkan.Enumerations
+import Graphics.Vulkan.Types
