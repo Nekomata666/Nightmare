6489e3f09d6e7aee379218a51424277a0a99c0bc
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-03 04:47:57 -0500

    Adds createVkDescriptorSetLayoutBinding

--- Graphics/Vulkan/Descriptor.hs
+++ Graphics/Vulkan/Descriptor.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module Graphics.Vulkan.Descriptor () where
+module Graphics.Vulkan.Descriptor (createVkDescriptorSetLayoutBinding) where
 
 
 import Data.Maybe
@@ -14,3 +14,16 @@ import Graphics.Utilities
 import Graphics.Vulkan.Data
 import Graphics.Vulkan.Enumerations
 import Graphics.Vulkan.Types
+
+
+-- Type aliases.
+type Binding                = Word32
+type DescriptorCount        = Word32
+
+createVkDescriptorSetLayoutBinding :: Binding -> VkDescriptorType -> DescriptorCount -> [VkShaderStageFlagBits] ->
+    Maybe VkSampler -> IO VkDescriptorSetLayoutBinding
+createVkDescriptorSetLayoutBinding b dT dC sSFB m = do
+    p <- fromMaybeIO m
+    return $ VkDescriptorSetLayoutBinding b dT dC sSF p
+    where
+        sSF = VkShaderStageFlags $ vkBits unVkShaderStageFlagBits sSFB
