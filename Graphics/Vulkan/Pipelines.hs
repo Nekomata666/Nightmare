b32228823c79e73adafe30aefe242bd03c4bccd6
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-03 04:27:29 -0500

    Adds createPipelineShaderStageInfo

--- Graphics/Vulkan/Pipelines.hs
+++ Graphics/Vulkan/Pipelines.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module Graphics.Vulkan.Pipelines () where
+module Graphics.Vulkan.Pipelines (createPipelineShaderStageInfo) where
 
 
 import Data.Maybe
@@ -16,3 +16,16 @@ import Graphics.Utilities
 import Graphics.Vulkan.Data
 import Graphics.Vulkan.Enumerations
 import Graphics.Vulkan.Types
+
+-- Type aliases.
+type Name                   = String
+
+createPipelineShaderStageInfo :: Ptr Void -> VkPipelineShaderStageCreateFlags -> VkShaderStageFlagBits -> VkShaderModule ->
+    Name -> Maybe VkSpecializationInfo -> IO VkPipelineShaderStageCreateInfo
+createPipelineShaderStageInfo v pSSCF sSF sM n Nothing = do
+    n' <- newCString n
+    return $ VkPipelineShaderStageCreateInfo structureTypePipelineShaderStageCreateInfo v pSSCF sSF sM n' nullPtr
+createPipelineShaderStageInfo v pSSCF sSF sM n (Just m) = alloca $ \pInfo -> do
+    n' <- newCString n
+    poke pInfo m
+    return $ VkPipelineShaderStageCreateInfo structureTypePipelineShaderStageCreateInfo v pSSCF sSF sM n' pInfo
