31a1d6aabc355d7e6d3e87d6d27898353c60be47
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-03 09:50:05 -0500

    Adds createVkPipelineCacheInfo

--- Graphics/Vulkan/Pipelines.hs
+++ Graphics/Vulkan/Pipelines.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module Graphics.Vulkan.Pipelines (createPipelineShaderStageInfo, createVkPipelineLayoutCreateInfo, vkCreatePipelineLayout) where
+module Graphics.Vulkan.Pipelines (createVkPipelineShaderStageInfo, createVkPipelineCacheInfo, createVkPipelineLayoutCreateInfo, vkCreatePipelineLayout) where
 
 
 import Data.Maybe
@@ -23,15 +23,17 @@ type PushConstantRangeCount = Word32
 type SetLayoutCount         = Word32
 
 foreign import ccall unsafe "vkCreatePipelineLayout"
-    c_vkCreatePipelineLayout :: VkDevice -> Ptr VkPipelineLayoutCreateInfo -> Ptr
-        VkAllocationCallbacks -> Ptr VkPipelineLayout -> IO VkResult
+    c_vkCreatePipelineLayout :: VkDevice -> Ptr VkPipelineLayoutCreateInfo -> Ptr VkAllocationCallbacks ->
+        Ptr VkPipelineLayout -> IO VkResult
 
-createPipelineShaderStageInfo :: Ptr Void -> VkPipelineShaderStageCreateFlags -> VkShaderStageFlagBits -> VkShaderModule ->
-    Name -> Maybe VkSpecializationInfo -> IO VkPipelineShaderStageCreateInfo
-createPipelineShaderStageInfo v pSSCF sSF sM n mVKSI = do
-    n' <- newCString n
-    pInfo <- fromMaybeIO mVKSI
-    return $ VkPipelineShaderStageCreateInfo structureTypePipelineShaderStageCreateInfo v pSSCF sSF sM n' pInfo
+createVkPipelineCacheInfo :: Ptr Void -> VkPipelineCacheCreateFlags -> FilePath -> IO VkPipelineCacheCreateInfo
+createVkPipelineCacheInfo v pCCF fP
+    | null fP = return $ VkPipelineCacheCreateInfo structureTypePipelineCacheCreateInfo v pCCF (CSize 0) nullPtr
+    | otherwise  = do
+        r <- openVulkanFile "CreatePipelineCacheInfo" fP
+        let p  = fst r :: Ptr Word32
+            cs = snd r
+        return $ VkPipelineCacheCreateInfo structureTypePipelineCacheCreateInfo v pCCF cs (castPtr p)
 
 createVkPipelineLayoutCreateInfo :: Ptr Void -> VkPipelineLayoutCreateFlags -> SetLayoutCount -> [VkDescriptorSetLayout] ->
     PushConstantRangeCount -> Maybe [VkPushConstantRange] -> IO VkPipelineLayoutCreateInfo
@@ -41,6 +43,13 @@ createVkPipelineLayoutCreateInfo v pLCF sLC dSL pCRC m = allocaArray i $ \pDSL -
         return $ VkPipelineLayoutCreateInfo structureTypePipelineLayoutCreateInfo v pLCF sLC pDSL pCRC pPCR
         where
             i = cast sLC
+
+createVkPipelineShaderStageInfo :: Ptr Void -> VkPipelineShaderStageCreateFlags -> VkShaderStageFlagBits -> VkShaderModule ->
+    Name -> Maybe VkSpecializationInfo -> IO VkPipelineShaderStageCreateInfo
+createVkPipelineShaderStageInfo v pSSCF sSF sM n mVKSI = do
+    n' <- newCString n
+    pInfo <- fromMaybeIO mVKSI
+    return $ VkPipelineShaderStageCreateInfo structureTypePipelineShaderStageCreateInfo v pSSCF sSF sM n' pInfo
 
 vkCreatePipelineLayout :: VkDevice -> VkPipelineLayoutCreateInfo -> IO VkPipelineLayout
 vkCreatePipelineLayout d pLCI = alloca $ \pPLCI ->
