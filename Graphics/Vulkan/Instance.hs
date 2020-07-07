8f44158e6a1c67c3700abfa1455e3192f1abb530
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-07 14:56:32 -0500

    Adds vkDestroyInstance

--- Graphics/Vulkan/Instance.hs
+++ Graphics/Vulkan/Instance.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, Safe #-}
 
-module Graphics.Vulkan.Instance (createVkApplicationInfo, createVkInstanceCreateInfo, vkCreateInstance) where
+module Graphics.Vulkan.Instance (createVkApplicationInfo, createVkInstanceCreateInfo, vkCreateInstance, vkDestroyInstance) where
 
 
 import Data.Void (Void)
@@ -31,6 +31,9 @@ type LayerNames         = [String]
 foreign import ccall unsafe "vkCreateInstance"
     c_vkCreateInstance :: Ptr VkInstanceCreateInfo -> Ptr VkAllocationCallbacks -> Ptr VkInstance -> IO VkResult
 
+foreign import ccall unsafe "vkDestroyInstance"
+    c_vkDestroyInstance :: VkInstance -> Ptr VkAllocationCallbacks -> IO ()
+
 createVkApplicationInfo :: Ptr Void -> ApplicationName -> ApplicationVersion -> EngineName -> EngineVersion -> APIVersion ->
     IO VkApplicationInfo
 createVkApplicationInfo v aN appV eN eV apiV = do
@@ -46,10 +49,12 @@ createVkInstanceCreateInfo v f aI lC lN eC eN = do
     eN' <- fromMaybeStringListIO eC eN
     return $ VkInstanceCreateInfo structureTypeInstanceCreateInfo v f aI' lC lN' eC eN'
 
-vkCreateInstance :: VkInstanceCreateInfo -> IO (VkInstance, VkResult)
+vkCreateInstance :: VkInstanceCreateInfo -> IO VkInstance
 vkCreateInstance vkInfo = alloca $ \pVkInfo ->
     alloca $ \pVkInstance -> do
         poke pVkInfo vkInfo
-        vkR <- c_vkCreateInstance pVkInfo nullPtr pVkInstance
-        inst <- peek pVkInstance
-        return (inst, vkR)
+        _ <- c_vkCreateInstance pVkInfo nullPtr pVkInstance
+        peek pVkInstance
+
+vkDestroyInstance :: VkInstance -> IO ()
+vkDestroyInstance i = c_vkDestroyInstance i nullPtr
