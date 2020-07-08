804ac04a25394b186a3106b419c967dc1b0a190c
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-08 11:38:50 -0500

    Adds sdl2VulkanCreateSurface

--- Main.hs
+++ Main.hs
@@ -15,13 +15,15 @@ main :: IO ()
 main = do
     _   <- sdl2Init init_Videos
     hW  <- sdl2CreateWindow "Nightmare" windowPos_Undefined windowPos_Undefined 1600 900 window_Vulkan
+    vkInst <- createInstance
+    vkSurf <- sdl2VulkanCreateSurface hW vkInst
 
     ----------------------------------------------------------------------------------------------------------------------------
     --
     -- Vulkan
     --
     ----------------------------------------------------------------------------------------------------------------------------
-    initialize
+    initialize vkInst
 
     threadDelay 9000000
 
