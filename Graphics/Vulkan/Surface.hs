bfd79624bcdf4c08aac6d14d7222efcdc631e380
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-08 15:46:22 -0500

    Creates Surface.hs

---
+++ Graphics/Vulkan/Surface.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE ForeignFunctionInterface, Safe #-}
+
+module Graphics.Vulkan.Surface () where
+
+
+import Data.Void (Void)
+import Data.Word (Word32, Word64)
+
+import Foreign
+
+import Graphics.Utilities
+
+import Graphics.Vulkan.Data
+import Graphics.Vulkan.Enumerations
+import Graphics.Vulkan.Types
