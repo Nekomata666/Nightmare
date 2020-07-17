2e98fa119e19eb68e1fd65ba11e0dd34f5d8bb93
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-07-17 08:49:58 -0500

    Creates Framebuffer.hs

---
+++ Graphics/Vulkan/Framebuffer.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ForeignFunctionInterface, Safe #-}
+
+module Graphics.Vulkan.Framebuffer () where
+
+
+import Data.Maybe   (Maybe)
+import Data.Void    (Void)
+import Data.Word    (Word32)
+
+import Foreign
+
+import Graphics.Utilities
+
+import Graphics.Vulkan.Data
+import Graphics.Vulkan.Enumerations
+import Graphics.Vulkan.Types
