0f808ee3dd13bcc177ee3f0699c3cd21dda6ca33
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-08-08 19:21:45 -0500

    Creates Fences.hs

---
+++ Graphics/Vulkan/Fences.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE ForeignFunctionInterface, Safe #-}
+
+module Graphics.Vulkan.Fences where
+
+
+import Data.Word (Word32, Word64)
+
+import Foreign
+
+import Graphics.Vulkan.Data
+import Graphics.Vulkan.Enumerations
+import Graphics.Vulkan.Types
