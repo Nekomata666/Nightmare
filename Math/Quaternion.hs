386fa1135da4e274ba78b4bb2cf536e23a321837
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-08-25 11:26:25 -0500

    Adds multiply

--- Math/Quaternion.hs
+++ Math/Quaternion.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE Safe #-}
 
-module Math.Quaternion (conjugate) where
+module Math.Quaternion (conjugate, multiply) where
 
 
 import Math.Data
@@ -12,3 +12,11 @@ conjugate (Quaternion x y z w) = Quaternion x' y' z' w
         x'  = negate x
         y'  = negate y
         z'  = negate z
+
+multiply :: Floating a => Quaternion a -> Quaternion a -> Quaternion a
+multiply (Quaternion x1 y1 z1 w1) (Quaternion x2 y2 z2 w2) = Quaternion x y z w
+    where
+        x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+        y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+        z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+        w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
