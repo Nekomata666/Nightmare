70df32c253728ff54a55728d2d8cdf4d90e93674
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2020-08-25 11:26:51 -0500

    Adds normalize

--- Math/Quaternion.hs
+++ Math/Quaternion.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE Safe #-}
 
-module Math.Quaternion (conjugate, multiply) where
+module Math.Quaternion (conjugate, multiply, normalize) where
 
 
 import Math.Data
+
+import Prelude hiding (length)
 
 
 conjugate :: Floating a => Quaternion a -> Quaternion a
@@ -13,6 +15,14 @@ conjugate (Quaternion x y z w) = Quaternion x' y' z' w
         y'  = negate y
         z'  = negate z
 
+length :: Floating a => Quaternion a -> a
+length (Quaternion x y z w) = sqrt $ x' + y' + z' + w'
+    where
+        x'  = x * x
+        y'  = y * y
+        z'  = z * z
+        w'  = w * w
+
 multiply :: Floating a => Quaternion a -> Quaternion a -> Quaternion a
 multiply (Quaternion x1 y1 z1 w1) (Quaternion x2 y2 z2 w2) = Quaternion x y z w
     where
@@ -20,3 +30,12 @@ multiply (Quaternion x1 y1 z1 w1) (Quaternion x2 y2 z2 w2) = Quaternion x y z w
         y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
         z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
         w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+
+normalize :: Floating a => Quaternion a -> Quaternion a
+normalize q@(Quaternion x y z w) = Quaternion x' y' z' w'
+    where
+        l   = length q
+        x'  = x / l
+        y'  = y / l
+        z'  = z / l
+        w'  = w / l
