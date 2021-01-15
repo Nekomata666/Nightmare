988c4051d86aadac816f67f12ee9ffe1cc7f9ebe
Nekomata666 <66758936+Nekomata666@users.noreply.github.com>
2021-01-15 17:17:19 -0500

    Rearranged shader code

--- Compile.sh
+++ Compile.sh
@@ -3,9 +3,9 @@ flags="-XSafe -O2 -dynamic -threaded"
 rts="-rtsopts"
 sdl2="-lSDL2"
 vulkan="-lvulkan -lVkLayer_khronos_validation"
-spirvFlags="-V120 --spirv-val"
+spirvFlags="--target-env vulkan1.2 --target-env spirv1.5 --spirv-val"
 
-glslangValidator $spirvFlags Shaders/Simple.vert -o Shaders/Simple.v.spv
-glslangValidator $spirvFlags Shaders/Simple.frag -o Shaders/Simple.f.spv
-glslangValidator $spirvFlags Shaders/Simple.comp -o Shaders/Simple.c.spv
+glslangValidator $spirvFlags Shaders/Violet/Rasterizer/Simple.vert -o Shaders/Violet/Rasterizer/Simple.v.spv
+glslangValidator $spirvFlags Shaders/Violet/Rasterizer/Simple.frag -o Shaders/Violet/Rasterizer/Simple.f.spv
+glslangValidator $spirvFlags Shaders/Violet/Compute/Tracer.comp -o Shaders/Violet/Compute/Tracer.c.spv
 ghc Main.hs $warnings $flags $sdl2 $vulkan $rts -outputdir ./Build/Release
